d6ad8eb0bc14efd8ddfef453ea2b8910a3084826
Eduardo Aguilera <laloao.2302@gmail.com>
2018-05-14 13:13:33 -0500

    Add excoveralls to see test coverage

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule ExBanking.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -22,7 +24,8 @@ defmodule ExBanking.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:moneyex, "~> 0.1.1"}
+      {:moneyex, "~> 0.1.1"},
+      {:excoveralls, "~> 0.8", only: :test}
     ]
   end
 end
